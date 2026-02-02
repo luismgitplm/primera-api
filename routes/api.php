f872e4b7415c3fcc4944eaec3acf14444012c2fa
luismgitplm <lrodnar@g.educaand.es>
2026-02-02 11:06:48 +0100

    .

--- routes/api.php
+++ routes/api.php
@@ -16,7 +16,7 @@ Route::post('/login', [AuthController::class, 'login']);
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::apiResource('posts', PostController::class);
+    Route::apiResource('productos', ApiProductoController::class);
 });
 
 
-Route::apiResource('productos', ApiProductoController::class);
